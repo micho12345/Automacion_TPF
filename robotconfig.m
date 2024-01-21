1b14d0af11bfbf42fab11f97e064f509c0c7d5f3
odevincenti <odevincenti@itba.edu.ar>
2024-01-21 05:21:55 -0300

    Emprolijo un poco, workspace anda

--- robotconfig.m
+++ robotconfig.m
@@ -6,33 +6,19 @@ L23 = sqrt(L(2)^2 + L(3)^2);
 Lee = 100;
 qlim = {[-pi/2, pi/2], [-pi/2, pi/2], [-pi/2, pi/2], [-pi/2, pi/2], [-pi/2, pi/2]};
 
-%%
-Peesym(1) = L23*cos(o1)*cos(o2) + L4*cos(o1)*cos(o2+o3) + L5*cos(o1)*cos(o2+o3+o4) - Leesym*(sin(o1)*sin(o5) - cos(o5)*cos(o1)*sin(o2+o3+o4));
-%Peesym(2) = - L23*sin(o1)*sin(o2) - L4*sin(o1)*sin(o2+o3) - L5*sin(o1)*sin(o2+o3+o4) + Leesym*(cos(o1)*sin(o5) + cos(o5)*sin(o1)*cos(o2+o3+o4));
-Peesym(3) = L1 + L23*sin(o2) + L4*sin(o2+o3) + L5*sin(o2+o3+o4) - Leesym*cos(o5)*cos(o2+o3+o4);
-
-%%
-%Pee = subs(Peesym, Lsym, L);
-%Pee = subs(Pee, Leesym, Lee);
-thlim = [90, 1, 1, 1, 1];
+%% Workspace
+thlim = [90, 90, 90, 90, 90];
 th1 = linspace(-double(thlim(1)),double(thlim(1)),90.0)*pi/180;
 th2 = linspace(-double(thlim(2)),double(thlim(2)),10.0)*pi/180;
 th3 = linspace(-double(thlim(3)),double(thlim(3)),10.0)*pi/180;
 th4 = linspace(-double(thlim(4)),double(thlim(4)),40.0)*pi/180;  
 th5 = linspace(-double(thlim(5)),double(thlim(5)),10.0)*pi/180;
-%[theta1, theta2, theta3, theta4] = ndgrid(th1,th2,th3,th4);
-%[o1, o2, o3, o4] = ndgrid(th1,th2,th3,th4);
 [o1, o2, o3, o4, o5] = ndgrid(th1,th2,th3,th4,th5);
 
 %%
 xw = L23*0.5*(cos(o1-o2) + cos(o1+o2)) + L(4)*0.5*(cos(o1-o2-o3) + cos(o1+o2+o3)) + L(5)*0.5*(cos(o1-o2-o3-o4) + cos(o1+o2+o3+o4)) - Lee*0.5*(cos(o1-o5) - cos(o1+o5) - 0.5*(sin(-o1+o2+o3+o4-o5) + sin(-o1+o2+o3+o4+o5) + sin(o1+o2+o3+o4-o5) + sin(o1+o2+o3+o4+o5)));
 yw = L23*0.5*(sin(o1+o2) + sin(o1-o2)) + L(4)*0.5*(sin(o1+o2+o3) + sin(o1-o2-o3)) + L(5)*0.5*( sin(o1+o2+o3+o4) + sin(o1-o2-o3-o4)) + Lee*0.5*((sin(o1+o5) + sin(-o1+o5)) + 0.5*(cos(o1-o2-o3-o4+o5) + cos(o1-o2-o3-o4-o5) - cos(o1+o2+o3+o4-o5) - cos(o1+o2+o3+o4+o5)));
 zw = L(1) + L23*sin(o2) + L(4)*sin(o2+o3) + L(5)*sin(o2+o3+o4) - Lee*0.5*(cos(o2+o3+o4-o5) + cos(o2+o3+o4+o5));
-
-
-% xw = - L23*0.5*(sin(o1+o2) + sin(-o1+o2)) - L(4)*0.5*(sin(o1+o2+o3) + sin(-o1+o2+o3)) - L(5)*0.5*(sin(o1+o2+o3+o4) + sin(-o1+o2+o3+o4)) - Lee*0.5*(cos(o1-o5)-cos(o1+o5)- 0.5*(cos(o1-o2-o3-o4-o5) + cos(o1-o2-o3-o4+o5) + cos(o1+o2+o3+o4-o5) + cos(o1+o2+o3+o4+o5)));
-% yw = - L23*0.5*(cos(o1-o2) - cos(o1+o2)) - L(4)*0.5*(cos(o1-o2-o3) - cos(o1+o2+o3)) - L(5)*0.5*(cos(o1-o2-o3-o4) - cos(o1+o2+o3+o4)) + Lee*0.5*(sin(o1+o5) + sin(-o1+o5) + 0.5*(sin(o1+o2+o3+o4+o5) + sin(o1-o2-o3-o4-o5) + sin(o1-o2-o3-o4+o5) + sin(o1+o2+o3+o4-o5)));
-% zw = L(1) + L23*cos(o2) + L(4)*cos(o2+o3) + L(5)*cos(o2+o3+o4) + Lee*0.5*(sin(o2+o3+o4+o5) + sin(o2+o3+o4-o5));
 
 %% Test
 Robot = createRobot(L, Lee);%, qlim);
