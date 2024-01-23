ee284b17255c6f9ebdbd076c729eafae3c1fe800
Ignacio Cutignola <cutignolai@gmail.com>
2024-01-23 02:39:07 -0300

    puta madre

--- V_mask.m
+++ V_mask.m
@@ -4,6 +4,86 @@ clc
 %% Importar imagenes sobre las que trabajamos
 im  =iread('c:\00 - Ignacio\Github\Automacion_TPF\Test Images\Foto1.jpg');
 
+
+
+%% Como primer paso vamos a aplicar una mascara VERDE
+% Este paso se hace primero para identificar los bordes en donde
+% se encuentra la linea roja
+
+%Valores del filtro
+green_mask_r_low = 60;
+green_mask_r_high = 110;
+green_mask_g_low = 60;
+green_mask_g_high = 150;
+green_mask_b_low = 60;
+green_mask_b_high = 150;
+
+
+I = im;
+
+% Defino los parametros de threshold de R
+channel1Min = green_mask_r_low;
+channel1Max = green_mask_r_high;
+
+% Defino los parametros de threshold de G
+channel2Min = green_mask_g_low;
+channel2Max = green_mask_g_high;
+
+% Defino los parametros de threshold de B
+channel3Min = green_mask_b_low;
+channel3Max = green_mask_b_high;
+
+
+% Se crea la mascara
+sliderBW = (I(:,:,1) >= channel1Min ) & (I(:,:,1) <= channel1Max) & ...
+    (I(:,:,2) >= channel2Min ) & (I(:,:,2) <= channel2Max) & ...
+    (I(:,:,3) >= channel3Min ) & (I(:,:,3) <= channel3Max);
+
+BW = sliderBW;
+GreenImage = im;
+GreenImage(repmat(~BW,[1 1 3])) = 0;
+
+
+% Ahora aplicamos apertura y cierre para tener una buena imagen de los
+% bordes, sin tanto ruido
+Gmon=imono(GreenImage);   
+Gblack=Gmon>0.9;
+closed_1 = iclose(Gblack, ones(2, 2));
+opened_1 = iopen(closed_1, ones(2, 2));
+borde_im = iclose(opened_1, kcircle(10));
+idisp(borde_im)
+
+%% Luego identificamos las lineas 
+
+edges = icanny(borde_im);
+h = Hough(edges, 'houghthresh', 0.6, 'suppress', 10)
+lines = h.lines();
+lines = lines.seglength(edges);
+k = find( lines.length > 1 & lines.length <= 32);
+
+
+idisp(borde_im)
+lines(k).plot('r')
+lines(k)
+
+
+%% Ahora encontramos las esquinas
+
+single_line = lines(1);
+
+single_values = [single_line.theta, single_line.rho, single_line.strength, single_line.length]
+
+single_line2 = lines(0);
+
+single_values2 = [single_line2.theta, single_line2.rho, single_line2.strength, single_line2.length]
+
+C = single_values.*single_values2
+
+[fil,col]=find(C == 2)      %%Fil y Col tienen las coordenadas de las interesecciones
+
+
+
+
 %% Aplico la mascara ROJA
 
 %Valores del filtro
@@ -136,6 +216,7 @@ k = find( lines.length > 1 & lines.length <= 32);
 idisp(borde_im)
 lines(k).plot('r')
 lines(k)
+
 
 
 
