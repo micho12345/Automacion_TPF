42c1a2d8ffef34d3435e42e37bef951afe9bf358
odevincenti <odevincenti@itba.edu.ar>
2024-02-15 15:49:26 -0300

    Emprolijo código

--- main.m
+++ main.m
@@ -19,12 +19,12 @@ table_start = [210, -100, 180];    % Posición del borde de la hoja (mm)
 table_end = [360, 100, 180];       % Posición en diagonal a la posición anterior
 table_size = [150, 200];           % Tamaño de la hoja
 
-%% Test
+%% Crear manipulador
 Robot = createRobot(L, Lee);
 q0 = deg2rad([0 90 -90 0]);    % Posicion inicial
 Robot.teach(q0)
 
-%% Test Workspace
+%% Espacio de trabajo
 thlim = [180, 110, 90, 100];
 %thlim = [50, 60, 60, 60];
 [xw, yw, zw] = workspace(L, Lee, thlim);
@@ -34,12 +34,12 @@ hold on
 plot3(xw(:),yw(:),zw(:))
 hold off
 
-%% Visión: Busco línea
+%% Visión: Busco linea
 points = findLine();
 r0 = [points(2), points(1), 0];
 rf = [points(4), points(3), 0];
 close all
 
-%% Draw Line
+%% Dibujo linea con manipulador
 z_offset = 20;
 drawLine(Robot, r0, rf, table_start, table_end, z_offset, q0)
